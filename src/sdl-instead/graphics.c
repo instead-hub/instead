63355d254dcf7679105dc7164069a1b1674b4b26
Peter Kosyh <p.kosyh@gmail.com>
2009-09-12 08:58:09 +0000

    sdl cleanups
    git-svn-id: svn+ssh://svn.code.sf.net/p/instead/code/trunk@184 2984d56d-8eb5-48f3-9f87-270ec957a5b2

--- src/sdl-instead/graphics.c
+++ src/sdl-instead/graphics.c
@@ -2134,7 +2134,7 @@ void gfx_change_screen(img_t src)
 
 int gfx_init(void)
 {
-	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER) < 0) {
+	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER | SDL_INIT_AUDIO) < 0) {
 		fprintf(stderr, "Couldn't initialize SDL: %s\n", SDL_GetError());
 		return -1;
   	}
