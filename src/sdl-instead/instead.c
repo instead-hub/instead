5c6ee07f6e59c58a2a92dc5da6d7bb3e4f8af6ae
Peter Kosyh <p.kosyh@gmail.com>
2009-10-12 17:02:59 +0000

    lua print reregister
    git-svn-id: svn+ssh://svn.code.sf.net/p/instead/code/trunk@352 2984d56d-8eb5-48f3-9f87-270ec957a5b2

--- src/sdl-instead/instead.c
+++ src/sdl-instead/instead.c
@@ -288,8 +288,29 @@ static int luaB_doencfile (lua_State *L) {
 	return lua_gettop(L) - n;
 }
 
+static int luaB_print (lua_State *L) {
+	int n = lua_gettop(L);  /* number of arguments */
+	int i;
+	lua_getglobal(L, "tostring");
+	for (i=1; i<=n; i++) {
+		const char *s;
+		lua_pushvalue(L, -1);  /* function to be called */
+		lua_pushvalue(L, i);   /* value to print */
+		lua_call(L, 1, 1);
+		s = lua_tostring(L, -1);  /* get result */
+		if (s == NULL)
+			return luaL_error(L, LUA_QL("tostring") " must return a string to "LUA_QL("print"));
+		if (i>1) fputs("\t", stdout);
+		fputs(s, stdout);
+		lua_pop(L, 1);  /* pop result */
+	}
+	fputs("\n", stdout);
+	return 0;
+}
+
 static const luaL_Reg base_funcs[] = {
 	{"doencfile", luaB_doencfile},
+	{"print", luaB_print}, /* for some mystic, it is needed in win version (with -debug) */
 	{NULL, NULL}
 };
 
