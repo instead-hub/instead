ec1e2e051a602fd75e0c85872ad0fe20283b7335
Peter Kosyh <p.kosyh@gmail.com>
2009-11-29 07:57:59 +0000

    extra headers removed
    git-svn-id: svn+ssh://svn.code.sf.net/p/instead/code/trunk@617 2984d56d-8eb5-48f3-9f87-270ec957a5b2

--- src/sdl-instead/externals.h
+++ src/sdl-instead/externals.h
@@ -11,24 +11,13 @@
 #include <iconv.h>
 #endif
 
-#include <stdio.h>
-#include <stdlib.h>
-
 #include <limits.h>
 // #include <pwd.h>
 
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
-
 #include <sys/stat.h>
 #include <sys/types.h>
-#include <stdio.h>
 #include <errno.h>
 #include <sys/fcntl.h>
-#include <string.h>
 #include <ctype.h>
-#include <string.h>
-#include <stdlib.h>
 #include <dirent.h>
 #include <time.h>
