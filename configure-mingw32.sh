86049066d744c1c933023327b65b5e066ed32f5f
Peter Kosyh <p.kosyh@gmail.com>
2010-06-01 18:56:51 +0000

    build system fix
    git-svn-id: svn+ssh://svn.code.sf.net/p/instead/code/trunk@825 2984d56d-8eb5-48f3-9f87-270ec957a5b2

---
+++ configure-mingw32.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+rm -f config.make
+rm -f Rules.make
+ln -sf Rules.mingw Rules.make
+rm -f sdl-instead
+ln -sf src/sdl-instead/sdl-instead sdl-instead
+echo "Ok. We are ready to build. Use this command:"
+echo "    \$ make -f Makefile.mingw setup"
