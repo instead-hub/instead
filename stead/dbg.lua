b58d9af0ca584ff3316b55a1b73464019fa99106
Peter Kosyh <p.kosyh@gmail.com>
2010-06-14 11:41:55 +0000

    dbg module added
    git-svn-id: svn+ssh://svn.code.sf.net/p/instead/code/trunk@904 2984d56d-8eb5-48f3-9f87-270ec957a5b2

---
+++ stead/dbg.lua
@@ -0,0 +1,77 @@
+-- add this: reuire "dbg"
+-- in your project
+-- for debug tools
+choose_location = dlg {
+	debug = true,
+	nam = 'Go to',
+	dsc = 'Select location.',
+	gen = function(s)
+		local k,v
+		objs(s):zap();
+		for k,v in pairs(_G) do
+			if isRoom(v) and not v.debug then
+				local n = call(v, 'nam');
+				put(phr(n, nil, [[return goto("]]..k..[[")]]), s);
+			end
+		end
+		put (phr('Exit',nil,'return back()'), s)
+	end
+}
+
+choose_object = dlg {
+	debug = true,
+	nam = 'Get object',
+	dsc = 'Select object to get.',
+	gen = function(s)
+		local k,v
+		objs(s):zap();
+		for k,v in pairs(_G) do
+			if isObject(v) and not isRoom(v) and not isPlayer(v) and not v.debug and not have(v) and not isStatus(v) then
+				local n = call(v, 'nam');
+				put(phr(n, '', k..':enable(); return take("'..k..'")'), s);
+			end
+		end
+		put (phr('Exit',nil, 'return back()'), s)
+	end
+}
+
+drop_object = dlg {
+	debug = true,
+	nam = 'Drop object',
+	dsc = 'Select object to drop.',
+	gen = function(s)
+		local k,v
+		objs(s):zap();
+		for k,v in pairs(_G) do
+			if isObject(v) and not isRoom(v) and not isPlayer(v) and not v.debug and have(v) then
+				local n = call(v, 'nam');
+				put (phr(k,'','drop("'..k..'","'..deref(from())..'")'), s)
+			end
+		end
+		put (phr('Exit',nil, 'return back()'), s)
+	end
+}
+
+debug_dlg = dlg {
+	debug = true, 
+	nam = 'Debug Tool',
+	dsc = 'Select tool.',
+	obj = {
+		[1]=phr('Go to location...', true ,[[pon(1); choose_location:gen(); return goto('choose_location')]]),
+		[2]=phr('Get object...', true , [[pon(2); choose_object:gen(); return goto('choose_object')]]),
+		[3]=phr('Put object...', true , [[pon(3); drop_object:gen(); return goto('drop_object')]]),
+		[4]=phr('Exit',nil , [[pon(4); return back()]]),
+	},
+};
+
+debug_tool = menu {
+	debug = true,
+	nam = txtb('debug'),
+	inv = function(s)
+		debug_dlg.__from__ = deref(here());
+		me().where = 'debug_dlg'; -- force to go
+		local r = call(debug_dlg, 'enter');
+		return r;
+	end
+};
+putf('debug_tool', me());
