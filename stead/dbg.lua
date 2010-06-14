9dc53cb7bb44fffec1494bc6aac819c0c9ce56ea
Peter Kosyh <p.kosyh@gmail.com>
2010-06-14 11:54:21 +0000

    dbg module added
    git-svn-id: svn+ssh://svn.code.sf.net/p/instead/code/trunk@905 2984d56d-8eb5-48f3-9f87-270ec957a5b2

--- stead/dbg.lua
+++ stead/dbg.lua
@@ -3,6 +3,7 @@
 -- for debug tools
 choose_location = dlg {
 	debug = true,
+	forcedsc = true,
 	nam = 'Go to',
 	dsc = 'Select location.',
 	gen = function(s)
@@ -11,15 +12,16 @@ choose_location = dlg {
 		for k,v in pairs(_G) do
 			if isRoom(v) and not v.debug then
 				local n = call(v, 'nam');
-				put(phr(n, nil, [[return goto("]]..k..[[")]]), s);
+				put(phr(n, true, [[return goto("]]..k..[[")]]), s);
 			end
 		end
-		put (phr('Exit',nil,'return back()'), s)
+		put (phr('Exit',true, 'return back()'), s)
 	end
 }
 
 choose_object = dlg {
 	debug = true,
+	forcedsc = true,
 	nam = 'Get object',
 	dsc = 'Select object to get.',
 	gen = function(s)
@@ -28,15 +30,16 @@ choose_object = dlg {
 		for k,v in pairs(_G) do
 			if isObject(v) and not isRoom(v) and not isPlayer(v) and not v.debug and not have(v) and not isStatus(v) then
 				local n = call(v, 'nam');
-				put(phr(n, '', k..':enable(); return take("'..k..'")'), s);
+				put(phr(n, true, k..':enable(); return take("'..k..'")'), s);
 			end
 		end
-		put (phr('Exit',nil, 'return back()'), s)
+		put (phr('Exit',true, 'return back()'), s)
 	end
 }
 
 drop_object = dlg {
 	debug = true,
+	forcedsc = true,
 	nam = 'Drop object',
 	dsc = 'Select object to drop.',
 	gen = function(s)
@@ -45,27 +48,29 @@ drop_object = dlg {
 		for k,v in pairs(_G) do
 			if isObject(v) and not isRoom(v) and not isPlayer(v) and not v.debug and have(v) then
 				local n = call(v, 'nam');
-				put (phr(k,'','drop("'..k..'","'..deref(from())..'")'), s)
+				put (phr(k, true, 'drop("'..k..'","'..deref(from())..'")'), s)
 			end
 		end
-		put (phr('Exit',nil, 'return back()'), s)
+		put (phr('Exit', true, 'return back()'), s)
 	end
 }
 
 debug_dlg = dlg {
-	debug = true, 
+	debug = true,
+	forcedsc = true,
 	nam = 'Debug Tool',
 	dsc = 'Select tool.',
 	obj = {
-		[1]=phr('Go to location...', true ,[[pon(1); choose_location:gen(); return goto('choose_location')]]),
-		[2]=phr('Get object...', true , [[pon(2); choose_object:gen(); return goto('choose_object')]]),
-		[3]=phr('Put object...', true , [[pon(3); drop_object:gen(); return goto('drop_object')]]),
-		[4]=phr('Exit',nil , [[pon(4); return back()]]),
+		[1]=phr('Go to location...', true, [[pon(1); choose_location:gen(); return goto('choose_location')]]),
+		[2]=phr('Get object...', true, [[pon(2); choose_object:gen(); return goto('choose_object')]]),
+		[3]=phr('Put object...', true, [[pon(3); drop_object:gen(); return goto('drop_object')]]),
+		[4]=phr('Exit',true , [[pon(4); return goto(from())]]),
 	},
 };
 
 debug_tool = menu {
 	debug = true,
+	forcedsc = true,
 	nam = txtb('debug'),
 	inv = function(s)
 		debug_dlg.__from__ = deref(here());
