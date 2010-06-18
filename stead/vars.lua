5e6257fe334fc9cea85139a5d4c7ffd31cf98b60
Peter Kosyh <p.kosyh@gmail.com>
2010-06-18 09:16:07 +0000

    better vars
    git-svn-id: svn+ssh://svn.code.sf.net/p/instead/code/trunk@932 2984d56d-8eb5-48f3-9f87-270ec957a5b2

--- stead/vars.lua
+++ stead/vars.lua
@@ -11,11 +11,11 @@ function (f, k, v, s, ...) -- k - key, v - value, s -- parent table
 	return f(k, v, s, unpack(arg))
 end)
 
-obj = inherit(obj, 
-function(v)
+function __vars_fill(v)
 	local k,o
-	local vars = {}
 	if type(v.var) == 'table' then
+		local k,o
+		local vars = {}
 		for k,o in pairs(v.var) do
 			if tonumber(k) and type(o) == 'string' then
 				stead.table.insert(vars, o)
@@ -26,5 +26,16 @@ function(v)
 		end
 		v.var = vars;
 	end
+end
+
+obj = inherit(obj, 
+function(v)
+	__vars_fill(v)
 	return v
+end)
+
+init = hook(init, 
+function (f, ...) -- k - key, v - value, s -- parent table
+	__vars_fill(_G)
+	return f(k, unpack(arg))
 end)
