891e40e68c8d7bf7387bdd9aa8c69ca588027528
Peter Kosyh <p.kosyh@gmail.com>
2010-06-19 10:14:25 +0000

    prefs and snapshots in modules
    git-svn-id: svn+ssh://svn.code.sf.net/p/instead/code/trunk@937 2984d56d-8eb5-48f3-9f87-270ec957a5b2

---
+++ stead/snapshot.lua
@@ -0,0 +1,46 @@
+game._snapshots = {}
+
+function make_snapshot(nr)
+	if not tonumber(nr) then nr = 0 end
+	local h = { };
+	h.txt = ''
+	h.write = function(s, ...)
+		local i
+		for i = 1, stead.table.maxn(arg) do
+			s.txt = s.txt .. tostring(arg[i]);
+		end
+	end
+	local old = game._snapshots; game._snapshots = nil
+	for_each_object(save_object, h);
+	save_object('game', game, h);
+	clearvar(_G);
+	game._snapshots = old
+	game._snapshots[nr] = h.txt;
+end
+
+function restore_snapshot(nr)
+	if not tonumber(nr) then nr = 0 end
+	local ss = game._snapshots
+	if not ss[nr] then return nil, true end -- nothing todo
+	stead_init();
+	game.lifes:zap();
+	dofile('main.lua');
+	if type(init) == 'function' then -- no hooks here!!!
+		init();
+	end
+	local f, err = loadstring(ss[nr]);
+	if not f then return end
+	local i,r = f();
+	game._snapshots = ss
+	if r then
+		return nil, false
+	end
+	i = do_ini(game);
+	RAW_TEXT = true
+	return i;
+end
+
+function delete_snapshot(nr)
+	if not tonumber(nr) then nr = 0 end
+	game._snapshots[nr] = nil
+end
