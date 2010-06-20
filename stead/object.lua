640046fedd358115730202c9560e78eba0963b27
Peter Kosyh <p.kosyh@gmail.com>
2010-06-20 15:47:49 +0000

    eXperemental object module
    git-svn-id: svn+ssh://svn.code.sf.net/p/instead/code/trunk@945 2984d56d-8eb5-48f3-9f87-270ec957a5b2

---
+++ stead/object.lua
@@ -0,0 +1,36 @@
+obj = inherit(obj, function(v)
+	if v.use then
+		v.use = hook(v.use, function(f, s, on, ...)
+			return f(s, ref(on), unpack(arg))
+		end)
+	end
+	if v.used then
+		v.used = hook(v.used, function(f, s, by, ...)
+			return f(s, ref(by), unpack(arg))
+		end)
+	end
+	return v
+end)
+
+room = inherit(room, function(v)
+	if v._enter then
+		v.enter = hook(v.enter, function(f, s, from, ...)
+			return f(s, ref(from), unpack(arg))
+		end)
+	end
+	if v.entered then
+		v.entered = hook(v.enter, function(f, s, from, ...)
+			return f(s, ref(from), unpack(arg))
+		end)
+	end
+	if v.exit then
+		v.exit = hook(v.exit, function(f, s, to, ...)
+			return f(s, ref(to), unpack(arg))
+		end)
+	end
+	if v.left then
+		v.left = hook(v.left, function(f, s, to, ...)
+			return f(s, ref(to), unpack(arg))
+		end)
+	end
+end)
