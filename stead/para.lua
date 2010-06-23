26ef89bd60d36dd95095e54e1b915bc312f52436
Peter Kosyh <p.kosyh@gmail.com>
2010-06-23 17:34:46 +0000

    para fix
    git-svn-id: svn+ssh://svn.code.sf.net/p/instead/code/trunk@974 2984d56d-8eb5-48f3-9f87-270ec957a5b2

--- stead/para.lua
+++ stead/para.lua
@@ -2,15 +2,16 @@ para_mod = obj {
 	nam = 'para',
 	ini = function(s)
 		s.time = stead.time()
-	end
+	end,
+	space = '    ',
 }
 
 stead.fmt = stead.hook(stead.fmt, function(f, ...)
 	local r = f(unpack(arg))
 	if type(r) == 'string' and stead.time() > para_mod.time then
 		para_mod.time = stead.time()
-		r = r:gsub('\n([^\n])', '<&para;>%1'):gsub('<&para;>[ \t]*', '\n'..txtnb('    '));
-		r = r:gsub('^',txtnb('    '))
+		r = r:gsub('\n([^\n])', '<&para;>%1'):gsub('<&para;>[ \t]*', '\n'..txtnb(para_mod.space));
+		r = r:gsub('^',txtnb(para_mod.space))
 	end
 	return r;
 end)
