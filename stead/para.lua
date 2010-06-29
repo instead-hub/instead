384bed45d227b714d78f8378f4fd6cf63e2812eb
Peter Kosyh <p.kosyh@gmail.com>
2010-06-29 16:23:52 +0000

    para fix, xroom better
    git-svn-id: svn+ssh://svn.code.sf.net/p/instead/code/trunk@995 2984d56d-8eb5-48f3-9f87-270ec957a5b2

--- stead/para.lua
+++ stead/para.lua
@@ -6,7 +6,7 @@ stead.fmt = stead.hook(stead.fmt, function(f, ...)
 	local r = f(unpack(arg))
 	if type(r) == 'string' and stead.state then
 		r = r:gsub('\n([^\n])', '<&para;>%1'):gsub('<&para;>[ \t]*', '\n'..txtnb(para_mod.space));
-		r = r:gsub('^',txtnb(para_mod.space))
+		r = r:gsub('^[ \t]*',txtnb(para_mod.space))
 	end
 	return r;
 end)
