564898ac2986408b1123bf1b39e2a489e5820a39
Peter Kosyh <p.kosyh@gmail.com>
2010-07-14 17:59:49 +0000

    fixes
    git-svn-id: svn+ssh://svn.code.sf.net/p/instead/code/trunk@1090 2984d56d-8eb5-48f3-9f87-270ec957a5b2

--- stead/snapshots.lua
+++ stead/snapshots.lua
@@ -1,6 +1,5 @@
 game._snapshots = {}
-
-function make_snapshot(nr)
+stead.make_snapshot = function(nr)
 	if not tonumber(nr) then nr = 0 end
 	local h = { };
 	h.txt = ''
@@ -21,7 +20,7 @@ function isSnapshot(nr)
 	return (game._snapshots[nr] ~= nil)
 end
 
-function restore_snapshot(nr)
+stead.restore_snapshot = function (nr)
 	if not tonumber(nr) then nr = 0 end
 	local ss = game._snapshots
 	if not ss[nr] then return nil, true end -- nothing todo
@@ -48,8 +47,33 @@ function restore_snapshot(nr)
 	return i;
 end
 
-function delete_snapshot(nr)
+stead.delete_snapshot = function(nr)
 	if not tonumber(nr) then nr = 0 end
 	game._snapshots[nr] = nil
 end
+
+function make_snapshot(nr)
+	if type(nr) ~= 'number' then
+		nr = 0
+	end
+	MAKE_SNAPSHOT = nr
+end
+
+function restore_snapshot(nr)
+	return stead.restore_snapshot(nr)
+end
+
+function delete_snapshot(nr)
+	return stead.delete_snapshot(nr);
+end
+
+iface.cmd = stead.hook(iface.cmd, function(f, ...)
+	local r,v = f(unpack(arg));
+	if MAKE_SNAPSHOT ~= nil then
+		stead.make_snapshot(MAKE_SNAPSHOT);
+		MAKE_SNAPSHOT = nil
+	end
+	if v == nil then return r end
+	return r,v
+end)
 -- vim:ts=4
