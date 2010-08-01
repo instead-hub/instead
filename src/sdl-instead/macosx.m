b9f4811ca6178c7f05b74b06bfa1c62a60d7100f
Peter Kosyh <p.kosyh@gmail.com>
2010-08-01 12:28:42 +0000

    macosx
    git-svn-id: svn+ssh://svn.code.sf.net/p/instead/code/trunk@1178 2984d56d-8eb5-48f3-9f87-270ec957a5b2

---
+++ src/sdl-instead/macosx.m
@@ -0,0 +1,25 @@
+#include <AppKit/NSOpenPanel.h>
+#include <Foundation/NSString.h>
+static char *file_name[4096];
+
+extern "C" char *macosx_open_file_dialog(void);
+
+char *macosx_open_file_dialog(void) 
+{
+	const char *filename;
+	NSOpenPanel * panel = [NSOpenPanel openPanel];
+	[panel setCanChooseDirectories:NO];
+	[panel setCanChooseFiles:YES];
+	[panel setAllowsMultipleSelection:NO];
+
+	if ([panel runModalForTypes:nil] == NSOKButton) {
+#ifdef __POWERPC__
+		filename = [[panel filename] cString];
+#else
+		filename = [[panel filename] cStringUsingEncoding:NSUTF8StringEncoding];
+#endif
+		strcpy(file_name, filename);
+		return file_name;
+	}
+	return NULL;
+}
