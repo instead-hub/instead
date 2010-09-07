9788c6a669e13aa514348ab00dd75e410ae83850
Peter Kosyh <p.kosyh@gmail.com>
2010-09-07 17:57:30 +0000

    build fix
    git-svn-id: svn+ssh://svn.code.sf.net/p/instead/code/trunk@1212 2984d56d-8eb5-48f3-9f87-270ec957a5b2

--- src/sdl-instead/sdl_iconv.h
+++ src/sdl-instead/sdl_iconv.h
@@ -1,11 +1,8 @@
 #ifndef _SDL_ICONV_H
 #define _SDL_ICONV_H
 
-#ifdef _HAVE_ICONV
-#include <SDL_config.h>
+#ifdef _SDL_ICONV
 
-#if defined(HAVE_ICONV) && !defined(HAVE_ICONV_H)
-#include <SDL_stdinc.h>
 #define iconv SDL_iconv
 #define iconv_t SDL_iconv_t
 #define iconv_open SDL_iconv_open
@@ -15,9 +12,7 @@
 
 #include <iconv.h>
 
-#endif /* _USE_SDL_ICONV */
-
-#endif /* _HAVE_ICONV */
+#endif /* _SDL_ICONV */
 
 #endif /* _SDL_ICONV_H */
 
