b15846ff4d550e8edaf43ec1b8c88cc60333336e
Peter Kosyh <p.kosyh@gmail.com>
2010-10-06 17:31:30 +0000

    optimization and wide theme
    git-svn-id: svn+ssh://svn.code.sf.net/p/instead/code/trunk@1277 2984d56d-8eb5-48f3-9f87-270ec957a5b2

--- src/sdl-instead/cache.h
+++ src/sdl-instead/cache.h
@@ -8,6 +8,7 @@ extern cache_t cache_init(int size, cache_free_fn);
 extern void cache_free(cache_t cache);
 extern int  cache_forget(cache_t cache, void *p);
 extern void cache_zap(cache_t cache);
+extern void cache_shrink(cache_t cache);
 extern void *cache_get(cache_t cache, const char *name);
 extern int cache_add(cache_t cache, const char *name, void *p);
 extern int cache_have(cache_t cache, void *p);
