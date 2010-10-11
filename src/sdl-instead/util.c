ca9bbf920a8353412beddbc7ac97e8a52796d9b3
Peter Kosyh <p.kosyh@gmail.com>
2010-10-11 11:36:38 +0000

    revert patch
    git-svn-id: svn+ssh://svn.code.sf.net/p/instead/code/trunk@1288 2984d56d-8eb5-48f3-9f87-270ec957a5b2

--- src/sdl-instead/util.c
+++ src/sdl-instead/util.c
@@ -51,7 +51,7 @@ char *strip(char *s)
 	return s;
 }
 
-int process_cmd(char *n, char *v, struct parser *cmd_parser)
+static int process_cmd(char *n, char *v, struct parser *cmd_parser)
 {
 	int i;
 	n = strip(n);
