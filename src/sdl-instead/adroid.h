30e1108c7c20e2b6ee50e2a0b82a6fc8ac301c5a
Peter Kosyh <p.kosyh@gmail.com>
2010-11-04 15:01:34 +0000

    android fix
    git-svn-id: svn+ssh://svn.code.sf.net/p/instead/code/trunk@1389 2984d56d-8eb5-48f3-9f87-270ec957a5b2

---
+++ src/sdl-instead/adroid.h
@@ -0,0 +1,13 @@
+#include <jni.h>
+#ifndef SDL_JAVA_PACKAGE_PATH
+#error You have to define SDL_JAVA_PACKAGE_PATH to your package path with dots replaced with underscores, for example "com_example_SanAngeles"
+#endif
+#define JAVA_EXPORT_NAME2(name,package) Java_##package##_##name
+#define JAVA_EXPORT_NAME1(name,package) JAVA_EXPORT_NAME2(name,package)
+#define JAVA_EXPORT_NAME(name) JAVA_EXPORT_NAME1(name,SDL_JAVA_PACKAGE_PATH)
+
+extern void JNICALL JAVA_EXPORT_NAME(MainActivity_nativeStop)(JNIEnv *env, jobject obj) {
+cfg_save();
+game_done(0);	
+exit(0);
+}
