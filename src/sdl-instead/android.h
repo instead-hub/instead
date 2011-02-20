16ad76fff8af7eba6627a8c20a3836faabff6ac1
Peter Kosyh <p.kosyh@gmail.com>
2011-02-20 08:20:32 +0000

    android fix
    git-svn-id: svn+ssh://svn.code.sf.net/p/instead/code/trunk@1464 2984d56d-8eb5-48f3-9f87-270ec957a5b2

--- src/sdl-instead/android.h
+++ src/sdl-instead/android.h
@@ -5,8 +5,11 @@
 #define JAVA_EXPORT_NAME2(name,package) Java_##package##_##name
 #define JAVA_EXPORT_NAME1(name,package) JAVA_EXPORT_NAME2(name,package)
 #define JAVA_EXPORT_NAME(name) JAVA_EXPORT_NAME1(name,SDL_JAVA_PACKAGE_PATH)
-extern void JNICALL JAVA_EXPORT_NAME(App_nativeStop)(JNIEnv *env, jobject obj) {
+extern void JNICALL JAVA_EXPORT_NAME(SDLActivity_nativeSave)(JNIEnv *env, jobject obj) {
 cfg_save();
+}
+
+extern void JNICALL JAVA_EXPORT_NAME(SDLActivity_nativeStop)(JNIEnv *env, jobject obj) {
 game_done(0);	
 exit(0);
 }
