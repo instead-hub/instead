f6c6c4bcfe23a1c2b8eb7ba001704266a182ed16
Peter Kosyh <p.kosyh@gmail.com>
2011-04-13 17:51:19 +0000

    cleanups in sprites
    git-svn-id: svn+ssh://svn.code.sf.net/p/instead/code/trunk@1554 2984d56d-8eb5-48f3-9f87-270ec957a5b2

--- stead/sprites.lua
+++ stead/sprites.lua
@@ -1,4 +1,21 @@
 require "theme"
+
+stead.font_load = font_load
+stead.font_free = font_free
+stead.sprite_alpha = sprite_alpha
+stead.sprite_dup = sprite_dup
+stead.sprite_scale = sprite_scale
+stead.sprite_rotate = sprite_rotate
+stead.sprite_text = sprite_text
+stead.sprite_text_size = sprite_text_size
+stead.sprite_draw = sprite_draw
+stead.sprite_copy = sprite_copy
+stead.sprite_fill = sprite_fill
+stead.sprite_load = sprite_load
+stead.sprite_free = sprite_free
+stead.sprite_size = sprite_size
+stead.sprites_free = sprites_free
+
 sprite = {
 	nam = 'sprites';
 	object_type = true;
@@ -6,7 +23,7 @@ sprite = {
 
 	ini = function(s)
 		if type(s.init) == 'function' then
-			sprites_free();
+			stead.sprites_free();
 			sprite.init();
 		end
 	end;
@@ -18,58 +35,58 @@ sprite = {
 		return 'screen'
 	end;
 	font = function(font, size)
-		return font_load(font, size);
+		return stead.font_load(font, size);
 	end;
 	free_font = function(font)
-		return font_free(key);
+		return stead.font_free(key);
 	end;
 	alpha = function(name, alpha)
-		return sprite_alpha(name, alpha);
+		return stead.sprite_alpha(name, alpha);
 	end;
 	dup = function(name)
-		return sprite_dup(name);
+		return stead.sprite_dup(name);
 	end;
 	scale = function(name, xs, ys)
-		return sprite_scale(name, xs, ys);
+		return stead.sprite_scale(name, xs, ys);
 	end;
 	rotate = function(name, angle)
-		return sprite_rotate(name, angle);
+		return stead.sprite_rotate(name, angle);
 	end;
 	text = function(font, text, col, style)
-		return sprite_text(font, text, col, style);
+		return stead.sprite_text(font, text, col, style);
 	end;
 	size = function(spr)
-		return sprite_size(spr);
+		return stead.sprite_size(spr);
 	end;
 	text_size = function(fnt, spr)
-		return sprite_text_size(fnt, spr);
+		return stead.sprite_text_size(fnt, spr);
 	end;
 	draw = function(s, fx, fy, fw, fh, d, x, y, alpha)
 		if d == nil and x == nil and y == nil then
-			return sprite_draw(s, 0, 0, 0, 0, fx, fy, fw, fh);
+			return stead.sprite_draw(s, 0, 0, 0, 0, fx, fy, fw, fh);
 		end
-		return sprite_draw(s, fx, fy, fw, fh, d, x, y, alpha);
+		return stead.sprite_draw(s, fx, fy, fw, fh, d, x, y, alpha);
 	end;
 	copy = function(s, fx, fy, fw, fh, d, x, y, alpha)
 		if d == nil and x == nil and y == nil then
-			return sprite_copy(s, 0, 0, 0, 0, fx, fy, fw, fh);
+			return stead.sprite_copy(s, 0, 0, 0, 0, fx, fy, fw, fh);
 		end
-		return sprite_copy(s, fx, fy, fw, fh, d, x, y, alpha);
+		return stead.sprite_copy(s, fx, fy, fw, fh, d, x, y, alpha);
 	end;
 	fill = function(d, x, y, w, h, col)
 		if h == nil and col == nil then
-			return sprite_fill(d, 0, 0, 0, 0, x);
+			return stead.sprite_fill(d, 0, 0, 0, 0, x);
 		end 
-		return sprite_fill(d, x, y, w, h, col);
+		return stead.sprite_fill(d, x, y, w, h, col);
 	end;
 	load = function(fname)
-		return sprite_load(fname);
+		return stead.sprite_load(fname);
 	end;
 	free = function(key)
-		return sprite_free(key);
+		return stead.sprite_free(key);
 	end;
 }
 
 stead.module_init(function()
-	sprites_free();
+	stead.sprites_free();
 end)
