0856fcc8da4031da54ab4b6edecef4288d35617c
Peter Kosyh <p.kosyh@gmail.com>
2011-06-17 16:49:24 +0000

    tutorial3 up
    git-svn-id: svn+ssh://svn.code.sf.net/p/instead/code/trunk@1702 2984d56d-8eb5-48f3-9f87-270ec957a5b2

--- games/tutorial3/main-it.lua
+++ games/tutorial3/main-it.lua
@@ -5,9 +5,9 @@ game.act = 'Non puoi.';
 game.inv = "Hmm... Sbagliato...";
 game.use = 'Non funziona...';
 
-game.pic = '../tutorial2/instead.png';
+game.pic = 'instead.png';
 
-set_music('../tutorial2/ramparts.mod');
+set_music('ramparts.mod');
 
 main = room {
 	nam = 'Tutorial',
