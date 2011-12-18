78f2639662e54a89963bd8eed5c2ab225ab4bf4e
Peter Kosyh <p.kosyh@gmail.com>
2011-12-18 06:42:30 +0000

    walk in docs
    git-svn-id: svn+ssh://svn.code.sf.net/p/instead/code/trunk@1839 2984d56d-8eb5-48f3-9f87-270ec957a5b2

--- doc/examples/debug.lua
+++ doc/examples/debug.lua
@@ -12,7 +12,7 @@ choose_location = dlg {
 		for k,v in pairs(_G) do
 			if isRoom(v) and not v.debug then
 				local n = call(v, 'nam');
-				put(phr(n, '', [[return goto("]]..k..[[")]]), s);
+				put(phr(n, '', [[return walk("]]..k..[[")]]), s);
 			end
 		end
 		put (phr('Exit','','back()'), s)
@@ -58,9 +58,9 @@ debug_dlg = dlg {
 	nam = 'Debug Tool',
 	dsc = 'Select tool.',
 	obj = {
-		[1]=phr('Go to location...', nil ,[[pon(1); choose_location:gen(); return goto('choose_location')]]),
-		[2]=phr('Get object...', nil , [[pon(2); choose_object:gen(); return goto('choose_object')]]),
-		[3]=phr('Put object...', nil , [[pon(3); drop_object:gen(); return goto('drop_object')]]),
+		[1]=phr('Go to location...', nil ,[[pon(1); choose_location:gen(); return walk('choose_location')]]),
+		[2]=phr('Get object...', nil , [[pon(2); choose_object:gen(); return walk('choose_object')]]),
+		[3]=phr('Put object...', nil , [[pon(3); drop_object:gen(); return walk('drop_object')]]),
 		[4]=phr('Exit', nil , [[pon(4); back()]]),
 	},
 };
