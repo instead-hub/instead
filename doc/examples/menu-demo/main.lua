e02be15ee803365070c9bb3d7ac13353d67d4db1
Peter Kosyh <p.kosyh@gmail.com>
2012-03-24 19:01:05 +0000

    new menu demo
    git-svn-id: svn+ssh://svn.code.sf.net/p/instead/code/trunk@1948 2984d56d-8eb5-48f3-9f87-270ec957a5b2

--- doc/examples/menu-demo/main.lua
+++ doc/examples/menu-demo/main.lua
@@ -1,37 +1,76 @@
-dofile("actions.lua");
-game.use = 'Не сработает...';
+instead_version "1.6.3"
+require "proxymenu"
+require "hideinv"
+
+game.forcedsc = true
+
+minv = obj_menu('С СОБОЙ', 'exam', false, true);
+mlook = obj_menu('ОСМОТРЕТЬ', 'exam', true);
+mtake = obj_menu('ВЗЯТЬ', 'take', true);
+mdrop = obj_menu('БРОСИТЬ', 'drop', false, true);
+meat = obj_menu('ЕСТЬ', 'eat', true, true);
+mpush = obj_menu('ТОЛКАТЬ', 'push', true);
+muse = use_menu('ИСПОЛЬЗОВАТЬ', 'useon', 'used', 'useit', true, true);
+mgive = use_menu('ОТДАТЬ', 'give', 'accept', false, true, true, true);
+mwalk = obj_menu('ИДТИ', 'walk', false, false, true);
+
+game.useit = 'Не помогло.'
+game.use = 'Не сработает.'
+game.give = 'Отдать? Ни за что!'
+game.eat = 'Не буду это есть.'
+game.drop = 'Еще пригодится.'
+game.exam = 'Ничего необычного.'
+game.take = 'Стоит ли это брать?'
+game.push = 'Ничего не произошло.'
+
+game.after_take = function(s, w)
+	take(w)
+end
+
+game.after_drop = function(s, w)
+	drop(w)
+end
+
+put(minv, me())
+put(mlook, me())
+put(mtake, me())
+put(mdrop, me())
+put(meat, me())
+put(mpush, me())
+put(muse, me())
+put(mgive, me())
+-- put(mwalk, me())
 
 status = stat {
-	Turns = 0,
+	_Turns = 0,
 	nam = function(s)
-		s.Turns = s.Turns + 1;
-		return 'Статус игрока: '..s.Turns..'^';
+		s._Turns = s._Turns + 1;
+		return 'Статус игрока: '..s._Turns..'^';
 	end
 };
+put(status, me());
 
-knife = iobj {
+knife = obj {
 	nam = 'ножик',
-	desc = 'На полу валяется {ножик}.',
+	dsc = 'На полу валяется ножик.',
 	exam = 'Бесполезный перочинный ножик.',
 }
 
 main = room {
 	nam = 'intro',
+	hideinv = "true",
 	dsc = 'Введение',
-	exit = function(s)
-		me().obj:add('status');
-		actions_init();
+	exit = function(s)		
 		inv():add('knife');
 	end,
 	obj = { vway('next','{Дальше}.', 'r1') }
 }
 
-cube = iobj {
+cube = obj {
 	nam = 'куб',
-	desc = 'В центре комнаты находится {куб}.',
+	dsc = 'В центре комнаты находится куб.',
 	take = 'Вы взяли куб',
 	exam = 'Мультифункциональный куб -- написано на кубе.',
-	inv = act.exam,
 	drop = 'Вы положили куб.',
 	useit = 'Как можно использовать куб?',
 	talk = 'Вы поговорили с кубом.',
@@ -50,12 +89,11 @@ cube = iobj {
 	used = 'Куб поюзан.',
 };
 
-sphere = iobj {
+sphere = obj {
 	nam = 'сфера',
-	desc = 'В центре комнаты находится {сфера}.',
+	dsc = 'В центре комнаты находится сфера.',
 	take = 'Вы взяли сферу',
 	exam = 'Мультифункциональная сфера -- написано на сфере.',
-	inv = act.exam,
 	drop = 'Вы положили сферу.',
 	useit = 'Как можно использовать сферу?',
 	talk = 'Вы поговорили с сферой.',
@@ -66,10 +104,10 @@ sphere = iobj {
 	close = 'Вы закрыли сферу.',
 	push = 'Вы толкаете сферу.',
 	give = function(s, w)
-		return 'Вы пытаетесь отдать сферу объекту: '..deref(w)..'.', false
+		return 'Вы пытаетесь отдать сферу объекту: '..nameof(w)..'.', false
 	end,
 	useon = function(s, w)
-		return 'Вы пытаетесь юзать сферу на объект: '..deref(w)..'. Получилось!'
+		return 'Вы пытаетесь юзать сферу на объект: '..nameof(w)..'. Получилось!'
 	end,
 	used = 'Сфера поюзана.',
 };
