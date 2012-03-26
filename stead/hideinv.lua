456c506e7fd9b7306557c2147a950a2018e3f6bd
Peter Kosyh <p.kosyh@gmail.com>
2012-03-26 19:04:21 +0000

    hideinv new
    git-svn-id: svn+ssh://svn.code.sf.net/p/instead/code/trunk@1957 2984d56d-8eb5-48f3-9f87-270ec957a5b2

--- stead/hideinv.lua
+++ stead/hideinv.lua
@@ -1,15 +1,23 @@
-room = stead.inherit(room, function(v)
-	v.entered = stead.hook(v.entered, function(f, s, ...)
-		if stead.call_bool(s, 'hideinv') then
-			me():disable_all();
+if stead.api_version >= "1.6.3" then
+	game.gui.hideinv = function(s)
+		if stead.call_bool(here(), 'hideinv') then
+			return true
 		end
-		return f(s, ...)
+	end
+else
+	room = stead.inherit(room, function(v)
+		v.entered = stead.hook(v.entered, function(f, s, ...)
+			if stead.call_bool(s, 'hideinv') then
+				me():disable_all();
+			end
+			return f(s, ...)
+		end)
+		v.left = stead.hook(v.left, function(f, s, ...)
+			if stead.call_bool(s, 'hideinv') then
+				me():enable_all();
+			end
+			return f(s, ...)
+		end)
+		return v
 	end)
-	v.left = stead.hook(v.left, function(f, s, ...)
-		if stead.call_bool(s, 'hideinv') then
-			me():enable_all();
-		end
-		return f(s, ...)
-	end)
-	return v
-end)
+end
