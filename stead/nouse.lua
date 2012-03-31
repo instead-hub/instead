4473b5a060e807d0d75a44b1093e725d03a5b1c5
Peter Kosyh <p.kosyh@gmail.com>
2012-03-31 08:18:10 +0000

    nouse better #3
    git-svn-id: svn+ssh://svn.code.sf.net/p/instead/code/trunk@1973 2984d56d-8eb5-48f3-9f87-270ec957a5b2

--- stead/nouse.lua
+++ stead/nouse.lua
@@ -1,11 +1,11 @@
 game.use = function(s, w, o)
 	local r, v
-	r, v = call(w, 'nouse', o);
+	r, v = stead.call(w, 'nouse', o);
 	if not r and not v then
-		r, v = call(o, 'noused', w);
+		r, v = stead.call(o, 'noused', w);
 	end
 	if not r and not v then
-		r, v = call(s, 'nouse', w, o)
+		r, v = stead.call(s, 'nouse', w, o)
 	end
-	return r,v
+	return r, v
 end
