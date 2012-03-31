58b3c44e0145d504a8944cbe607157ba5e26db28
Peter Kosyh <p.kosyh@gmail.com>
2012-03-31 08:16:12 +0000

    nouse better
    git-svn-id: svn+ssh://svn.code.sf.net/p/instead/code/trunk@1971 2984d56d-8eb5-48f3-9f87-270ec957a5b2

--- stead/nouse.lua
+++ stead/nouse.lua
@@ -1,9 +1,9 @@
 game.use = function(s, w, o)
 	if w.nouse then
-		return call(w, 'nouse');
+		return call(w, 'nouse', o);
 	end
 	if o.noused then
-		return call(o, 'noused');
+		return call(o, 'noused', w);
 	end
-	return call(s, 'nouse')
+	return call(s, 'nouse', w, o)
 end
