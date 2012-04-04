9d01e660b327f7b6d9e5c22a6e2bbd8484073a00
Peter Kosyh <p.kosyh@gmail.com>
2012-04-04 18:24:04 +0000

    hideinv->noinv
    git-svn-id: svn+ssh://svn.code.sf.net/p/instead/code/trunk@1996 2984d56d-8eb5-48f3-9f87-270ec957a5b2

--- stead/hideinv.lua
+++ stead/hideinv.lua
@@ -1,3 +1,4 @@
+local attr = "hideinv"
 if stead.api_version >= "1.6.3" then
 	game.gui.hideinv = function(s)
 		if stead.call_bool(game, 'hideinv') then
@@ -7,20 +8,20 @@ if stead.api_version >= "1.6.3" then
 			return true
 		end
 	end
-else
-	room = stead.inherit(room, function(v)
-		v.entered = stead.hook(v.entered, function(f, s, ...)
-			if stead.call_bool(s, 'hideinv') then
-				me():disable_all();
-			end
-			return f(s, ...)
-		end)
-		v.left = stead.hook(v.left, function(f, s, ...)
-			if stead.call_bool(s, 'hideinv') then
-				me():enable_all();
-			end
-			return f(s, ...)
-		end)
-		return v
-	end)
+	attr = "noinv"
 end
+room = stead.inherit(room, function(v)
+	v.entered = stead.hook(v.entered, function(f, s, ...)
+		if stead.call_bool(s, attr) then
+			me():disable_all();
+		end
+		return f(s, ...)
+	end)
+	v.left = stead.hook(v.left, function(f, s, ...)
+		if stead.call_bool(s, attr) then
+			me():enable_all();
+		end
+		return f(s, ...)
+	end)
+	return v
+end)
