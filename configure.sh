b085a40ea51ca7abb492fcf6260bf8c3594b9149
Peter Kosyh <p.kosyh@gmail.com>
2014-01-07 12:31:30 +0000

    LUA setenv support
    git-svn-id: svn+ssh://svn.code.sf.net/p/instead/code/trunk@2371 2984d56d-8eb5-48f3-9f87-270ec957a5b2

--- configure.sh
+++ configure.sh
@@ -45,8 +45,11 @@ fi
 
 
 echo -n "Checking pkg-config --cflags lua[5.1|5.2|51|52|jit]..."
-
-lua_ver="luajit lua5.1 lua5.2 lua lua-5.1 lua-5.2 lua51 lua52"
+if [ "x$LUA" = "x" ]; then
+	lua_ver="lua5.1 lua5.2 lua lua-5.1 lua-5.2 lua51 lua52 luajit"
+else
+	lua_ver=$LUA
+fi
 
 for v in $lua_ver; do
 	if pkg-config --cflags "$v" >/dev/null 2>&1; then
