a7f1fd241c6786c1dc73396f88ece47d154bc2d9
Peter Kosyh <p.kosyh@gmail.com>
2014-01-25 20:15:57 +0400

    goodbye static inlines

--- src/sdl-instead/list.c
+++ src/sdl-instead/list.c
@@ -3,6 +3,140 @@
 #include <stdlib.h>
 #include "list.h"
 
+void list_head_init(struct list_head *h)
+{
+	h->n.next = h->n.prev = &h->n;
+}
+
+void list_add(struct list_head *h, struct list_node *n)
+{
+	n->next = h->n.next;
+	n->prev = &h->n;
+	h->n.next->prev = n;
+	h->n.next = n;
+	(void)list_debug(h);
+}
+
+void list_add_tail(struct list_head *h, struct list_node *n)
+{
+	n->next = &h->n;
+	n->prev = h->n.prev;
+	h->n.prev->next = n;
+	h->n.prev = n;
+	(void)list_debug(h);
+}
+
+bool list_empty(const struct list_head *h)
+{
+	(void)list_debug(h);
+	return h->n.next == &h->n;
+}
+
+void list_del(struct list_node *n)
+{
+	(void)list_debug_node(n);
+	n->next->prev = n->prev;
+	n->prev->next = n->next;
+#ifdef CCAN_LIST_DEBUG
+	/* Catch use-after-del. */
+	n->next = n->prev = NULL;
+#endif
+}
+
+void list_del_from(struct list_head *h, struct list_node *n)
+{
+#ifdef CCAN_LIST_DEBUG
+	{
+		/* Thorough check: make sure it was in list! */
+		struct list_node *i;
+		for (i = h->n.next; i != n; i = i->next)
+			assert(i != &h->n);
+	}
+#endif /* CCAN_LIST_DEBUG */
+
+	/* Quick test that catches a surprising number of bugs. */
+	assert(!list_empty(h));
+	list_del(n);
+}
+
+const void *list_top_(const struct list_head *h, size_t off)
+{
+	if (list_empty(h))
+		return NULL;
+	return (const char *)h->n.next - off;
+}
+
+const void *list_pop_(const struct list_head *h, size_t off)
+{
+	struct list_node *n;
+
+	if (list_empty(h))
+		return NULL;
+	n = h->n.next;
+	list_del(n);
+	return (const char *)n - off;
+}
+
+const void *list_tail_(const struct list_head *h, size_t off)
+{
+	if (list_empty(h))
+		return NULL;
+	return (const char *)h->n.prev - off;
+}
+
+void list_append_list(struct list_head *to,
+				    struct list_head *from)
+{
+	struct list_node *from_tail = list_debug(from)->n.prev;
+	struct list_node *to_tail = list_debug(to)->n.prev;
+
+	/* Sew in head and entire list. */
+	to->n.prev = from_tail;
+	from_tail->next = &to->n;
+	to_tail->next = &from->n;
+	from->n.prev = to_tail;
+
+	/* Now remove head. */
+	list_del(&from->n);
+	list_head_init(from);
+}
+
+void list_prepend_list(struct list_head *to,
+				     struct list_head *from)
+{
+	struct list_node *from_tail = list_debug(from)->n.prev;
+	struct list_node *to_head = list_debug(to)->n.next;
+
+	/* Sew in head and entire list. */
+	to->n.next = &from->n;
+	from->n.prev = &to->n;
+	to_head->prev = from_tail;
+	from_tail->next = to_head;
+
+	/* Now remove head. */
+	list_del(&from->n);
+	list_head_init(from);
+}
+
+void *list_node_to_off_(struct list_node *node, size_t off)
+{
+	return (void *)((char *)node - off);
+}
+
+struct list_node *list_node_from_off_(void *ptr, size_t off)
+{
+	return (struct list_node *)((char *)ptr + off);
+}
+
+void *list_entry_or_null(const struct list_head *h,
+				       const struct list_node *n,
+				       size_t off)
+{
+	if (n == &h->n)
+		return NULL;
+	return (char *)n - off;
+}
+
 static void *corrupt(const char *abortstr,
 		     const struct list_node *head,
 		     const struct list_node *node,
--- src/sdl-instead/list.h
+++ src/sdl-instead/list.h
@@ -330,10 +330,7 @@ struct list_node *list_check_node(const struct list_node *n,
  *	list_head_init(&parent->children);
  *	parent->num_children = 0;
  */
-static inline void list_head_init(struct list_head *h)
-{
-	h->n.next = h->n.prev = &h->n;
-}
+void list_head_init(struct list_head *h);
 
 /**
  * list_add - add an entry at the start of a linked list.
@@ -348,14 +345,7 @@ static inline void list_head_init(struct list_head *h)
  *	list_add(&parent->children, &child->list);
  *	parent->num_children++;
  */
-static inline void list_add(struct list_head *h, struct list_node *n)
-{
-	n->next = h->n.next;
-	n->prev = &h->n;
-	h->n.next->prev = n;
-	h->n.next = n;
-	(void)list_debug(h);
-}
+void list_add(struct list_head *h, struct list_node *n);
 
 /**
  * list_add_tail - add an entry at the end of a linked list.
@@ -367,14 +357,7 @@ static inline void list_add(struct list_head *h, struct list_node *n)
  *	list_add_tail(&parent->children, &child->list);
  *	parent->num_children++;
  */
-static inline void list_add_tail(struct list_head *h, struct list_node *n)
-{
-	n->next = &h->n;
-	n->prev = h->n.prev;
-	h->n.prev->next = n;
-	h->n.prev = n;
-	(void)list_debug(h);
-}
+void list_add_tail(struct list_head *h, struct list_node *n);
 
 /**
  * list_empty - is a list empty?
@@ -385,11 +368,8 @@ static inline void list_add_tail(struct list_head *h, struct list_node *n)
  * Example:
  *	assert(list_empty(&parent->children) == (parent->num_children == 0));
  */
-static inline bool list_empty(const struct list_head *h)
-{
-	(void)list_debug(h);
-	return h->n.next == &h->n;
-}
+
+bool list_empty(const struct list_head *h);
 
 /**
  * list_del - delete an entry from an (unknown) linked list.
@@ -405,16 +385,8 @@ static inline bool list_empty(const struct list_head *h)
  *	list_del(&child->list);
  *	parent->num_children--;
  */
-static inline void list_del(struct list_node *n)
-{
-	(void)list_debug_node(n);
-	n->next->prev = n->prev;
-	n->prev->next = n->next;
-#ifdef CCAN_LIST_DEBUG
-	/* Catch use-after-del. */
-	n->next = n->prev = NULL;
-#endif
-}
+
+void list_del(struct list_node *n);
 
 /**
  * list_del_from - delete an entry from a known linked list.
@@ -430,21 +402,8 @@ static inline void list_del(struct list_node *n)
  *	list_del_from(&parent->children, &child->list);
  *	parent->num_children--;
  */
-static inline void list_del_from(struct list_head *h, struct list_node *n)
-{
-#ifdef CCAN_LIST_DEBUG
-	{
-		/* Thorough check: make sure it was in list! */
-		struct list_node *i;
-		for (i = h->n.next; i != n; i = i->next)
-			assert(i != &h->n);
-	}
-#endif /* CCAN_LIST_DEBUG */
 
-	/* Quick test that catches a surprising number of bugs. */
-	assert(!list_empty(h));
-	list_del(n);
-}
+void list_del_from(struct list_head *h, struct list_node *n);
 
 /**
  * list_entry - convert a list_node back into the structure containing it.
@@ -478,12 +437,7 @@ static inline void list_del_from(struct list_head *h, struct list_node *n)
 #define list_top(h, type, member)					\
 	((type *)list_top_((h), list_off_(type, member)))
 
-static inline const void *list_top_(const struct list_head *h, size_t off)
-{
-	if (list_empty(h))
-		return NULL;
-	return (const char *)h->n.next - off;
-}
+const void *list_top_(const struct list_head *h, size_t off);
 
 /**
  * list_pop - remove the first entry in a list
@@ -502,16 +456,7 @@ static inline const void *list_top_(const struct list_head *h, size_t off)
 #define list_pop(h, type, member)					\
 	((type *)list_pop_((h), list_off_(type, member)))
 
-static inline const void *list_pop_(const struct list_head *h, size_t off)
-{
-	struct list_node *n;
-
-	if (list_empty(h))
-		return NULL;
-	n = h->n.next;
-	list_del(n);
-	return (const char *)n - off;
-}
+const void *list_pop_(const struct list_head *h, size_t off);
 
 /**
  * list_tail - get the last entry in a list
@@ -530,12 +475,7 @@ static inline const void *list_pop_(const struct list_head *h, size_t off)
 #define list_tail(h, type, member) \
 	((type *)list_tail_((h), list_off_(type, member)))
 
-static inline const void *list_tail_(const struct list_head *h, size_t off)
-{
-	if (list_empty(h))
-		return NULL;
-	return (const char *)h->n.prev - off;
-}
+const void *list_tail_(const struct list_head *h, size_t off);
 
 /**
  * list_for_each - iterate through a list.
@@ -644,22 +584,8 @@ static inline const void *list_tail_(const struct list_head *h, size_t off)
  *	assert(list_empty(&parent->children));
  *	parent->num_children = 0;
  */
-static inline void list_append_list(struct list_head *to,
-				    struct list_head *from)
-{
-	struct list_node *from_tail = list_debug(from)->n.prev;
-	struct list_node *to_tail = list_debug(to)->n.prev;
-
-	/* Sew in head and entire list. */
-	to->n.prev = from_tail;
-	from_tail->next = &to->n;
-	to_tail->next = &from->n;
-	from->n.prev = to_tail;
-
-	/* Now remove head. */
-	list_del(&from->n);
-	list_head_init(from);
-}
+void list_append_list(struct list_head *to,
+				    struct list_head *from);
 
 /**
  * list_prepend_list - empty one list into the start of another.
@@ -674,22 +600,8 @@ static inline void list_append_list(struct list_head *to,
  *	assert(list_empty(&parent->children));
  *	parent->num_children = 0;
  */
-static inline void list_prepend_list(struct list_head *to,
-				     struct list_head *from)
-{
-	struct list_node *from_tail = list_debug(from)->n.prev;
-	struct list_node *to_head = list_debug(to)->n.next;
-
-	/* Sew in head and entire list. */
-	to->n.next = &from->n;
-	from->n.prev = &to->n;
-	to_head->prev = from_tail;
-	from_tail->next = to_head;
-
-	/* Now remove head. */
-	list_del(&from->n);
-	list_head_init(from);
-}
+void list_prepend_list(struct list_head *to,
+				     struct list_head *from);
 
 /**
  * list_for_each_off - iterate through a list of memory regions.
@@ -772,14 +684,8 @@ static inline void list_prepend_list(struct list_head *to,
 	list_del_from(h, list_node_from_off_((n), (off)))
 
 /* Offset helper functions so we only single-evaluate. */
-static inline void *list_node_to_off_(struct list_node *node, size_t off)
-{
-	return (void *)((char *)node - off);
-}
-static inline struct list_node *list_node_from_off_(void *ptr, size_t off)
-{
-	return (struct list_node *)((char *)ptr + off);
-}
+void *list_node_to_off_(struct list_node *node, size_t off);
+struct list_node *list_node_from_off_(void *ptr, size_t off);
 
 /* Get the offset of the member, but make sure it's a list_node. */
 #define list_off_(type, member)					\
@@ -797,12 +703,7 @@ static inline struct list_node *list_node_from_off_(void *ptr, size_t off)
 #endif
 
 /* Returns member, or NULL if at end of list. */
-static inline void *list_entry_or_null(const struct list_head *h,
+void *list_entry_or_null(const struct list_head *h,
 				       const struct list_node *n,
-				       size_t off)
-{
-	if (n == &h->n)
-		return NULL;
-	return (char *)n - off;
-}
+				       size_t off);
 #endif /* CCAN_LIST_H */
