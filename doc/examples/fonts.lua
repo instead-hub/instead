4337ae7af3001c55270645058cdff4e1dbfa56e4
Peter Kosyh <p.kosyh@gmail.com>
2014-10-26 10:41:38 +0400

    new fonts version

--- doc/examples/fonts.lua
+++ doc/examples/fonts.lua
@@ -58,16 +58,24 @@ function font(name, size, scale)
 				t = 0
 			end
 			while true do
-				s, e = txt:find("[ \t]+", ss);
+				s, e = txt:find("[ \t\n^]+", ss);
 				local w = txt:sub(ss, s);
-				if w and w ~= '' then
+				local c
+				if w then
+					if s then
+						c = txt:sub(s, s)
+					end
+					w = w:gsub("[ \t%^\n]+$", "");
+				end
+				if w and w ~= '' and w ~= '\n' then
 					st:cache_add(w, color, t)
 					res = res .. img(st:cache_get(w, color, t).img);
 				end
 				if not e then break end
-				ss = e + 1
-				res = res .. ' ';
-			end	
+				ss = s + 1
+				if not c then c = '' end
+				res = res .. c;
+			end
 			return res;
 		end;
 		cache_add = function(s, w, color, t, key, time)
