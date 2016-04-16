adac446b0285ae815f55a1a6033d23d0df511d53
Peter Kosyh <p.kosyh@gmail.com>
2016-04-16 13:47:21 +0300

    ios __private__ data

--- src/sdl-instead/ios.h
+++ src/sdl-instead/ios.h
@@ -1,11 +1,11 @@
 #ifndef _IOS_H_INCLUDED
 #define _IOS_H_INCLUDED
 #define VERSION "2.4.0"
-#define STEAD_PATH "stead"
-#define THEMES_PATH "themes"
-#define GAMES_PATH "games"
-#define ICON_PATH "icon"
-#define LANG_PATH "lang"
+#define STEAD_PATH "__private__/stead"
+#define THEMES_PATH "__private__/themes"
+#define GAMES_PATH "__private__/games"
+#define ICON_PATH "__private__/icon"
+#define LANG_PATH "__private__/lang"
 extern int setup_inbox(void);
 extern void ios_cfg_init(void);
 #endif
