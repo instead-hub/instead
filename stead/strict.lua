cd59ea372dfb82a451c57e42ed6d73be6a32f05e
Peter Kosyh <p.kosyh@gmail.com>
2016-11-26 20:11:26 +0300

    strict module up

--- stead/strict.lua
+++ stead/strict.lua
@@ -1,15 +1,21 @@
+function declare(n)
+	if type(n) == 'string' then
+		stead.rawset(_G, n, false)
+	end
+end
+
 setmetatable(_G, {
 	__index = function(_, n)
 		local f = stead.getinfo(2, "S").source
 		if f:byte(1) == 0x40 and not f:find("/stead/", 1, true) then
-			print ("Uninitialized global variable: "..n.." in: "..f:sub(2))
+			error ("Uninitialized global variable: "..n.." in: "..f:sub(2), 2)
 		end
 	end;
 	__newindex = function(t, k, v)
 		local f = stead.getinfo(2, "S").source
-		if f:byte(1) == 0x40 and not f:find("/stead/", 1, true) and not isObject(v) then
-			print ("Set uninitialized variable: "..k.." in: "..f:sub(2))
+		if f:byte(1) == 0x40 and not f:find("/stead/", 1, true) and type(v) ~= 'function' and not isObject(v) then
+			error ("Set uninitialized variable: "..k.." in: "..f:sub(2), 2)
 		end
-		rawset(t, k, v)
+		stead.rawset(t, k, v)
 	end
 })
