c63bf60f81f52cafa8fd1dfe2ae4fed5947b3fdf
Peter Kosyh <p.kosyh@gmail.com>
2016-11-26 22:34:13 +0300

    strict

--- stead/strict.lua
+++ stead/strict.lua
@@ -16,14 +16,22 @@ if not RELEASE then
 setmetatable(_G, {
 	__index = function(_, n)
 		local f = stead.getinfo(2, "S").source
-		if f:byte(1) == 0x40 and not f:find("/stead/", 1, true) then
-			print ("Uninitialized global variable: "..n.." in: "..f:sub(2))
+		if f:byte(1) == 0x40 then
+			if f:find("/stead/", 1, true) then
+				print ("Uninitialized global variable: "..n.." in: "..f:sub(2))
+			else
+				error ("Uninitialized global variable: "..n.." in: "..f:sub(2), 2)
+			end
 		end
 	end;
 	__newindex = function(t, k, v)
 		local f = stead.getinfo(2, "S").source
-		if f:byte(1) == 0x40 and not f:find("/stead/", 1, true) and type(v) ~= 'function' and not isObject(v) then
-			print ("Set uninitialized variable: "..k.." in: "..f:sub(2))
+		if f:byte(1) == 0x40and and type(v) ~= 'function' and not isObject(v) then
+			if f:find("/stead/", 1, true) then
+				print ("Set uninitialized variable: "..k.." in: "..f:sub(2))
+			else
+				error ("Set uninitialized variable: "..k.." in: "..f:sub(2), 2)
+			end
 		end
 		stead.rawset(t, k, v)
 	end
