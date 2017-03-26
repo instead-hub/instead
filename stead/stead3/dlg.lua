7c952018b9e5de6484daa31f67f0be8b290c67a5
Peter Kosyh <p.kosyh@gmail.com>
2017-03-26 19:42:55 +0300

    dlg __onact->ph_onact

--- stead/stead3/dlg.lua
+++ stead/stead3/dlg.lua
@@ -56,7 +56,7 @@ std.dlg = std.class({
 		end
 		return std.par(std.scene_delim, title or false, lact or false, dsc)
 	end;
-	__onact = function(s, w) -- show dsc by default
+	ph_onact = function(s, w) -- show dsc by default
 		if not std.phrase_show then
 			return
 		end
@@ -319,7 +319,10 @@ std.phr = std.class({
 	act = function(s, ...)
 		local n = s
 --		s = s:__alias()
-		local onact = s:inroom():__onact(s)
+		local onact, v = std.call(std.here(), 'ph_onact', s)
+		if not v then
+			return onact
+		end
 		local w = s:where()
 		if w and w.only then -- only one choice
 			for i = 1, #w.obj do
