9739040f99734c44531e1cc6bc862c96750de0ea
Peter Kosyh <p.kosyh@gmail.com>
2017-10-16 14:02:23 +0300

    do not be so difficult: (revert last patch)

--- stead/stead3/stead.lua
+++ stead/stead3/stead.lua
@@ -1860,9 +1860,8 @@ std.world = std.class({
 			end
 			local o1 = std.ref(cmd[2])
 			local o2 = std.ref(cmd[3])
-			if not std.is_system(o1) then
-				o1 = s.player:srch(o1)
-			end
+			o1 = s.player:srch(o1)
+
 			if not o1 then
 				return nil, false -- wrong input
 			end
