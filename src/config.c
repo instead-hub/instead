c58a5a166386a005ae4ff91a3d7a875dcdc91c60
Peter Kosyh <p.kosyh at gmail dot com>
2017-10-28 15:14:23 +0300

    font scale for modern pdas

--- src/config.c
+++ src/config.c
@@ -25,9 +25,9 @@
 #include "externals.h"
 #include "internals.h"
 
-#if defined(ANDROID) || defined(S60) || defined(_WIN32_WCE) || defined(MAEMO) || defined(WINRT)
+#if defined(S60) || defined(_WIN32_WCE) || defined(MAEMO)
 int opt_fsize = 12;
-#elif defined(IOS)
+#elif defined(IOS) || defined(SAILFISH) || defined(ANDROID) || defined(WINRT)
 int opt_fsize = 5;
 #else
 int opt_fsize = 0;
