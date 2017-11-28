416ba90fe7a8931f7be94f6192d4db1eaf6c7a6a
Peter Kosyh <p.kosyh@gmail.com>
2017-11-28 09:02:13 +0300

    io.open for relative paths is always taken from gamedir

--- stead/stead2/ext/paths.lua
+++ stead/stead2/ext/paths.lua
@@ -1,1 +1,9 @@
 -- nothing todo in stead2 ;)
+local instead_gamepath = instead_gamepath
+io.open = stead.hook(io.open, function(f, path, acc, ...)
+	if stead.type(path) == 'string' and path:sub(1, 1) ~= '/' and not
+		(path:sub(2, 2) == ':' and path:find("^[A-Z]", 1) == 1) then
+		path = instead_gamepath()..'/'..path
+	end
+	return f(path, acc, ...)
+end)
--- stead/stead3/ext/paths.lua
+++ stead/stead3/ext/paths.lua
@@ -5,3 +5,11 @@ instead.savepath = instead_savepath
 std.savepath = instead_savepath
 instead.gamepath = instead_gamepath
 instead.exepath = instead_exepath
+
+io.open = stead.hook(io.open, function(f, path, acc, ...)
+	if std.type(path) == 'string' and path:sub(1, 1) ~= '/' and not
+		(path:sub(2, 2) == ':' and path:find("^[A-Z]", 1) == 1) then
+		path = instead.gamepath()..'/'..path
+	end
+	return f(path, acc, ...)
+end)
