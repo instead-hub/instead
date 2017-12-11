331c447b9ef2dedb2e43d5959ead2a44025e03ad
Peter Kosyh <p.kosyh@gmail.com>
2017-12-11 18:29:03 +0300

    dprint -> std.dprint

--- stead/stead3/stead.lua
+++ stead/stead3/stead.lua
@@ -2693,7 +2693,7 @@ local iface = std.obj {
 	cmd = function(self, inp)
 		local cmd = cmd_parse(inp)
 		if std.debug_input then
-			dprint("* input: ", inp)
+			std.dprint("* input: ", inp)
 		end
 		if not cmd then
 			return "Error in cmd arguments", false
@@ -2707,7 +2707,7 @@ local iface = std.obj {
 		end
 		r = iface:fmt(r, v) -- to force fmt
 		if std.debug_output then
-			dprint("* output: ", r, v)
+			std.dprint("* output: ", r, v)
 		end
 		return r, v
 	end;
