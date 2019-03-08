52a04f24fa4716af85c98897b845b4476a082e48
Peter Kosyh <p.kosyh@gmail.com>
2019-03-08 09:21:53 +0300

    emscripten build script

--- contrib/instead-em-build.sh
+++ contrib/instead-em-build.sh
@@ -274,6 +274,7 @@ emcc -O2 sdl-instead.bc lib/libz.a lib/libiconv.so lib/liblua.a lib/libSDL2_ttf.
 -s QUANTUM_SIZE=4 \
 -s BINARYEN_TRAP_MODE=clamp \
 -s WASM=1 \
+-s PRECISE_F32=1 \
 -s USE_OGG=1 -s USE_VORBIS=1 -s USE_LIBPNG=1 \
 -o instead-em.html -s SAFE_HEAP=0  -s TOTAL_MEMORY=167772160 -s ALLOW_MEMORY_GROWTH=1 \
 --post-js post.js  \
