7be906adea25472dec22de6d656b68af5c027efa
Peter Kosyh <p.kosyh@gmail.com>
2021-05-13 15:23:04 +0300

    tiny sandbox stead3

--- src/tiny/tiny.lua
+++ src/tiny/tiny.lua
@@ -6,6 +6,18 @@
 
 if API == 'stead3' then
 	dofile 'tiny3.lua'
+	local instead = std '@instead'
+	instead.savepath = function() return "./" end
+	std.mod_start(function()
+		std.mod_init(function()
+			std.rawset(_G, 'instead', instead)
+			require "ext/sandbox"
+		end)
+		local mp = std.ref '@metaparser'
+		if mp then
+			mp.msg.CUTSCENE_MORE = '^'..mp.msg.CUTSCENE_HELP
+		end
+	end)
 	return
 end
 
