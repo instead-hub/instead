7a7120d855790a95430ffdfc6a95cac5a76a0f42
Peter Kosyh <p.kosyh@gmail.com>
2021-09-08 20:57:25 +0300

    fix gamepict scale

--- src/game.c
+++ src/game.c
@@ -1436,7 +1436,8 @@ img_t	game_pict_scale(img_t img, int ww, int hh)
 	}
 	if (scale < 0)
 		scale = 0;
-	scale *= (game_theme.img_scale * game_theme.scale);
+	if (game_theme.img_scale * game_theme.scale > 1.0f)
+		scale *= (game_theme.img_scale * game_theme.scale);
 	img2 = gfx_scale(img, scale, scale, SCALABLE_THEME_SMOOTH);
 	gfx_free_image(img);
 	return img2;
