7ef093adb626fae794ab379ff4e8fca9c360aa02
Peter Kosyh <pkosyh@yandex.ru>
2023-08-27 21:47:14 +0300

    Update instead-em-build.sh
    emscripten fix

--- contrib/instead-em-build.sh
+++ contrib/instead-em-build.sh
@@ -69,10 +69,10 @@ fi
 # zlib
 cd $WORKSPACE
 if ! test -r .stamp_zlib; then
-rm -rf zlib-1.2.13/
-[ -f zlib-1.2.13.tar.gz ] || wget -nv 'http://zlib.net/zlib-1.2.13.tar.gz'
-tar xf zlib-1.2.13.tar.gz
-cd zlib-1.2.13
+rm -rf zlib-1.3/
+[ -f zlib-1.3.tar.gz ] || wget -nv 'http://zlib.net/zlib-1.3.tar.gz'
+tar xf zlib-1.3.tar.gz
+cd zlib-1.3
 emconfigure ./configure --prefix=$WORKSPACE
 emmake make install
 touch ../.stamp_zlib
